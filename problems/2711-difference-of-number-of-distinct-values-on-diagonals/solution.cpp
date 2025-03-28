5ac7c6fc5ed8d65d18c565530361191f3cbcaf0d
Reese <reese_duan@outlook.com>
2025-03-28 22:04:33 +0800

    No.2711 20250328

--- problems/2711-difference-of-number-of-distinct-values-on-diagonals/solution.cpp
+++ problems/2711-difference-of-number-of-distinct-values-on-diagonals/solution.cpp
@@ -5,6 +5,31 @@
 class Solution {
     public:
         vector<vector<int>> differenceOfDistinctValues(vector<vector<int>>& grid) {
-            
+            int m = grid.size();
+            int n = grid[0].size();
+
+            vector<vector<int>> ans(m, vector<int>(n, 0));
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    unordered_set<int> topLeft;
+                    unordered_set<int> bottomRight;
+                    int x = i - 1;
+                    int y = j - 1;
+                    while (x >= 0 && y >= 0) {
+                        topLeft.insert(grid[x][y]);
+                        x--;
+                        y--;
+                    }
+                    x = i + 1;
+                    y = j + 1;
+                    while (x < m && y < n) {
+                        bottomRight.insert(grid[x][y]);
+                        x++;
+                        y++;
+                    }
+                    ans[i][j] = abs((int)topLeft.size() - (int)bottomRight.size());
+                }
+            }
+            return ans;
         }
 };
